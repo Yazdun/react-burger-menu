2cbc00caed0abfdc92c62142d1fa50ba3daf7ddf
Yazdun <yazduntube@gmail.com>
2023-11-04 16:55:11 +0330

    commit

--- src/components/nav-desktop.tsx
+++ src/components/nav-desktop.tsx
@@ -1,22 +1,5 @@
-import { routes } from "../routes";
+// import { routes } from "../routes";
 
 export const NavDesktop = () => {
-  return (
-    <ul className="hidden lg:flex lg:items-center gap-5 text-sm">
-      {routes.map((route) => {
-        const { Icon, href, title } = route;
-        return (
-          <li>
-            <a
-              href={href}
-              className="flex items-center gap-1 hover:text-neutral-400 transition-all"
-            >
-              <Icon />
-              {title}
-            </a>
-          </li>
-        );
-      })}
-    </ul>
-  );
+  return <div>desktop</div>;
 };
--- src/components/nav-mobile.tsx
+++ src/components/nav-mobile.tsx
@@ -1,74 +1,10 @@
-"use client";
-
-import { useClickAway } from "react-use";
-import { useRef } from "react";
-import { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import { Squash as Hamburger } from "hamburger-react";
-import { routes } from "../routes";
+// import { useClickAway } from "react-use";
+// import { useRef } from "react";
+// import { useState } from "react";
+// import { AnimatePresence, motion } from "framer-motion";
+// import { Squash as Hamburger } from "hamburger-react";
+// import { routes } from "../routes";
 
 export const NavMobile = () => {
-  const [isOpen, setOpen] = useState(false);
-  const ref = useRef(null);
-
-  useClickAway(ref, () => setOpen(false));
-
-  return (
-    <div ref={ref} className="lg:hidden ">
-      <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
-      <AnimatePresence>
-        {isOpen && (
-          <motion.div
-            {...framerMenu}
-            className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 to-neutral-950 from-neutral-900 via-neutral-950 bg-gradient-to-t border-b border-b-white/20"
-          >
-            <ul className="grid gap-2">
-              {routes.map((route, idx) => {
-                const { Icon } = route;
-
-                return (
-                  <motion.li
-                    {...framerMenuItem(idx)}
-                    key={route.title}
-                    className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700"
-                  >
-                    <a
-                      onClick={() => setOpen((prev) => !prev)}
-                      className={
-                        "flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
-                      }
-                      href={route.href}
-                    >
-                      <span className="flex gap-1 text-lg">{route.title}</span>
-                      <Icon className="text-xl" />
-                    </a>
-                  </motion.li>
-                );
-              })}
-            </ul>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
-  );
-};
-
-const framerMenu = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  exit: { opacity: 0 },
-  transition: { duration: 0.2 },
-};
-
-const framerMenuItem = (delay: number) => {
-  return {
-    initial: { scale: 0, opacity: 0 },
-    animate: { scale: 1, opacity: 1 },
-    transition: {
-      type: "spring",
-      stiffness: 260,
-      damping: 20,
-      delay: 0.1 + delay / 10,
-    },
-  };
+  return <div>nav mobile</div>;
 };
