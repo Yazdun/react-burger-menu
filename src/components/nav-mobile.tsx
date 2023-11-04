1494355826d0f9d48ec847243d23de613b69cde5
Yazdun <yazduntube@gmail.com>
2023-11-04 17:11:24 +0330

    update nav mobile

--- src/components/nav-mobile.tsx
+++ src/components/nav-mobile.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useClickAway } from "react-use";
 import { useRef } from "react";
 import { useState } from "react";
@@ -19,8 +17,11 @@ export const NavMobile = () => {
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            {...framerMenu}
-            className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 to-neutral-950 from-neutral-900 via-neutral-950 bg-gradient-to-t border-b border-b-white/20"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20"
           >
             <ul className="grid gap-2">
               {routes.map((route, idx) => {
@@ -28,7 +29,14 @@ export const NavMobile = () => {
 
                 return (
                   <motion.li
-                    {...framerMenuItem(idx)}
+                    initial={{ scale: 0, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{
+                      type: "spring",
+                      stiffness: 260,
+                      damping: 20,
+                      delay: 0.1 + idx / 10,
+                    }}
                     key={route.title}
                     className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700"
                   >
@@ -51,24 +59,4 @@ export const NavMobile = () => {
       </AnimatePresence>
     </div>
   );
-};
-
-const framerMenu = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  exit: { opacity: 0 },
-  transition: { duration: 0.2 },
-};
-
-const framerMenuItem = (delay: number) => {
-  return {
-    initial: { scale: 0, opacity: 0 },
-    animate: { scale: 1, opacity: 1 },
-    transition: {
-      type: "spring",
-      stiffness: 260,
-      damping: 20,
-      delay: 0.1 + delay / 10,
-    },
-  };
 };
